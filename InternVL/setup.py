28ca7c681fd7030292fc6dd888fcf2afb7b4f679
shivangjhalani <shivang2004jhalani@gmail.com>
2025-07-04 22:51:02 +0530

    fix: Convert InternVL from submodule to regular directory

---
+++ InternVL/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='internvl',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A local installation of the InternVL library.',
+) 
